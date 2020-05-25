45aae2732ec000356938a1fc3b0eb252db62032e
Andy Agus <redagus@gmail.com>
2020-05-25 16:24:27 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,6 +10,11 @@ def select_winner(passengers)
 
 end
 
-
-
-
+winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
+end
+ 
+winner
